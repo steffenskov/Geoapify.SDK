475c0c80da9b6cdcc880bbddaa92967f82eea38a
steffenskov <steffen@ckode.dk>
2025-12-28 11:42:08 +0100

    chore: Missed a ton of files

---
+++ src/Geoapify.DependencyInjection/Setup.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using Geoapify.SDK.Client;
+using Geoapify.SDK.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+public static class GeoapifySetup
+{
+	public static IServiceCollection AddGeoapify(this IServiceCollection services, string apiKey)
+	{
+		services.AddHttpClient<GeoapifyClient>(client =>
+		{
+			client.BaseAddress = new Uri("https://api.geoapify.com/v1/");
+			client.DefaultRequestHeaders.Add("Accept", "application/json");
+			client.DefaultRequestHeaders.UserAgent.ParseAdd("Geoapify.SDK/1.0 (+https://github.com/steffenskov/Geoapify.SDK)");
+		}).ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
+		{
+			AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate,
+			AllowAutoRedirect = true
+		});
+		services.Configure<GeoapifyConfiguration>(options =>
+		{
+			options.ApiKey = apiKey;
+		});
+		services.AddTransient<IGeoapifyClient, GeoapifyClient>();
+		return services;
+	}
+}
---
+++ src/Geoapify.SDK/Client/GeoapifyClient.cs
@@ -0,0 +1,25 @@
+﻿using Geoapify.SDK.Configuration;
+using Geoapify.SDK.Geocoding;
+using Geoapify.SDK.Http;
+using Microsoft.Extensions.Options;
+
+namespace Geoapify.SDK.Client;
+
+public class GeoapifyClient : IGeoapifyClient
+{
+	public GeoapifyClient(IHttpClientFactory httpClientFactory, string apiKey)
+	{
+		Geocoding = new GeocodingModule(new HttpClientFactoryWrapper(httpClientFactory, nameof(GeoapifyClient)), apiKey);
+	}
+
+	public GeoapifyClient(IHttpClientFactory httpClientFactory, IOptions<GeoapifyConfiguration> options) : this(httpClientFactory, options.Value.ApiKey)
+	{
+	}
+
+	public IGeocodingModule Geocoding { get; }
+}
+
+public interface IGeoapifyClient
+{
+	IGeocodingModule Geocoding { get; }
+}
